253c62cf8c2eddba19b86352c7ac446d89fd3e20
陈桂东 <9542762+cgd-cgd@user.noreply.gitee.com>
2022-08-14 20:05:48 +0800

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import Request from './request/request'
 
 const myRequest = new Request({
 	withCredentials: true,
-	baseURL: 'https://netease-cloud-music-api-six-navy.vercel.app/',
+	baseURL: '/api',
 	timeout: 10000,
 })
 export default myRequest
